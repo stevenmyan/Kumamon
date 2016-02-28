8c6b143deb7ceb609275ca5dbc74f4d4a58b4451
jingli430 <jingli430@gmail.com>
2016-02-27 17:29:50 -0800

    add feature of laoding data to mongodb

--- appstore/appstore/pipelines.py
+++ appstore/appstore/pipelines.py
@@ -8,16 +8,17 @@
 from scrapy.contrib.pipeline.images import ImagesPipeline
 from scrapy.exceptions import DropItem
 from scrapy import Request
+import pymongo
 
 
-class AppstorePipeline(object):
+class AppstoreWritePipeline(object):
     def __init__(self):
         self.file = open('appstore.dat', 'wb')
 
     def process_item(self, item, spider):
         val = "{0}\t{1}\t{2}\n{3}\n\n".format(item['appid'], item['title'], item['intro'], item['recommended'])
         self.file.write(val)
-        return item # must return (or drop) the item at the end of each pipeline, this item will be used in subsequent item pipeline
+        return item  # must return (or drop) the item at the end of each pipeline, this item will be used in subsequent item pipeline
 
 
 class AppstoreImagesPipeline(ImagesPipeline):
@@ -30,4 +31,43 @@ class AppstoreImagesPipeline(ImagesPipeline):
         if not image_paths:
             raise DropItem("Item contains no images")
         item['image_paths'] = image_paths
+        return item
+
+
+class AppstoreMongodbPipeline(object):
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        """
+        return an instance of this pipeline
+        crawler.settings --> settings.py
+        get mongo_uri & mongo_database from settings.py
+        :param crawler:
+        :return: crawler instance
+        """
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        """
+        process data here before loading to mongodb
+        :param item:
+        :param spider:
+        :return: item
+        """
+        collection_name = item.__class__.__name__  # use itemName as the collectionName
+        #self.db[collection_name].remove({}) # clean this collection when new crawling starts
+        self.db[collection_name].insert(dict(item))
         return item
